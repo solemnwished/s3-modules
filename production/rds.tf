dde5199e7cc0a226469d46c7e06bca53dadfef76
solemnwished <namduynguyenit@gmail.com>
2022-09-03 00:53:06 +0700

    add RDS production

---
+++ production/rds.tf
@@ -0,0 +1,73 @@
+module "db" {
+  source  = "terraform-aws-modules/rds/aws"
+
+  identifier = "proddb"
+
+  engine            = "mysql"
+  engine_version    = "5.7.38"
+  instance_class    = "db.m5.large"
+  allocated_storage = 5
+
+  db_name  = "ndnmysql${var.env}"
+  username = "user"
+  port     = "3306"
+
+  iam_database_authentication_enabled = true
+
+  vpc_security_group_ids = [module.ec2_security_group.security_group_id]
+
+  maintenance_window = "Mon:00:00-Mon:03:00"
+  backup_window      = "03:00-06:00"
+
+  # Enhanced Monitoring - see example for details on how to create the role
+  # by yourself, in case you don't want to create it automatically
+  monitoring_interval = "30"
+  monitoring_role_name = "MyRDSMonitoringRole-PROD"
+  create_monitoring_role = true
+
+  tags = {
+    Owner       = "user"
+    Environment = "${var.env}"
+  }
+
+  # DB subnet group
+  create_db_subnet_group = true
+  subnet_ids             = ["subnet-0428294ea97df5ae5", "subnet-0c2fc975f8826a252"]
+
+  # DB parameter group
+  family = "mysql5.7"
+
+  # DB option group
+  major_engine_version = "5.7"
+
+  # Database Deletion Protection
+  deletion_protection = true
+
+  parameters = [
+    {
+      name = "character_set_client"
+      value = "utf8mb4"
+    },
+    {
+      name = "character_set_server"
+      value = "utf8mb4"
+    }
+  ]
+
+  options = [
+    {
+      option_name = "MARIADB_AUDIT_PLUGIN"
+
+      option_settings = [
+        {
+          name  = "SERVER_AUDIT_EVENTS"
+          value = "CONNECT"
+        },
+        {
+          name  = "SERVER_AUDIT_FILE_ROTATIONS"
+          value = "37"
+        },
+      ]
+    },
+  ]
+}
